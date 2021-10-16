74275ea4509cb680248ed1cc647dd83b43b0356a
CDGodoy <cdgodoy17@gmail.com>
2021-10-15 21:03:00 -0300

    first commit

---
+++ src/test/java/br/com/godoydev/festa/GestaoFestaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.godoydev.festa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoFestaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
